301d1a91457180ce5e73fbb797656814f3fd3b2a
HarshMarolia <maroliaharsh@gmail.com>
2023-09-23 18:18:38 +0530

    Project complete

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [task, setTask] = useState([{ fname: "", lname: 0 }])
   const [data, setData] = useState({
     yourName: '',
     yourNumber: '',
@@ -18,12 +19,12 @@ export default function Home() {
     customerName: '',
     companyEmail: '',
     items: {
-      "A new website from scratch on the latest technology.": 13500,
-      "SPA for speed optimization.": 1000,
-      "Functional form section, to get photos over mail.": 1500,
-      "Bilingual functionality": 2000,
-      "2 Plugins/extensions installation": 0,
-      "2 revisions and upto 5 pages in a product": 0
+      // "A new website from scratch on the latest technology.": 13500,
+      // "SPA for speed optimization.": 1000,
+      // "Functional form section, to get photos over mail.": 1500,
+      // "Bilingual functionality": 2000,
+      // "2 Plugins/extensions installation": 0,
+      // "2 revisions and upto 5 pages in a product": 0
     },
     totalPrice: ''
   });
@@ -39,6 +40,18 @@ export default function Home() {
   // container function to generate the Invoice
   const generateInvoice = e => {
     e.preventDefault();
+    // logic to insert data into items section
+    // var temp = JSON.parse(task);
+    var totalVal = 0;
+    task.forEach((item, index) => {
+      // console.log(`Item ${index + 1}`);
+      // console.log(`First Name: ${item.fname}`);
+      // console.log(`Last Name: ${item.lname}`);
+      // console.log('---');
+      data.items[item.fname] = item.lname;
+      totalVal += Number(item.lname);
+    });
+    data.totalPrice = totalVal;
     // send a post request with the name to our API endpoint
     const fetchData = async () => {
       const currentURL = window.location.href;
@@ -64,6 +77,27 @@ export default function Home() {
 
     saveAsPDF();
   };
+
+  // Dynamic form section
+  
+  const addNewFields = (e) => {
+    e.preventDefault();
+    setTask([...task, { fname: "", lname: 0 }])
+  }
+
+  const handleChange = (e, i) => {
+    const { name, value } = e.target
+    const onchangeVal = [...task]
+    onchangeVal[i][name] = value
+    setTask(onchangeVal)
+  }
+
+  const handleDelete = (e, i) => {
+    e.preventDefault();
+    const deleteVal = [...task]
+    deleteVal.splice(i, 1)
+    setTask(deleteVal)
+  }
 
   return (
     <div className={styles.container}>
@@ -191,9 +225,22 @@ export default function Home() {
             />
 
             {/* Items section */}
+            <h3>Items</h3>
+            <button onClick={addNewFields} className={styles.button1}>Add New Item</button>
+            <div className={styles.flexibox}>
+            {
+              task.map((val, i) =>
+                <div className={styles.flexiboxchild}>
+                  <input type='text' name="fname" value={val.fname} onChange={(e) => handleChange(e, i)} />
+                  <input type='number' name="lname" value={val.lname} onChange={(e) => handleChange(e, i)} />
+                  <button onClick={(e) => handleDelete(e, i)}>Delete</button>
+                </div>
+              )
+            }
+            </div>
+            {/* <p>{JSON.stringify(task)}</p> */}
 
             {/* {items section ends} */}
-
             {/* Form ended */}
           </div>
 
